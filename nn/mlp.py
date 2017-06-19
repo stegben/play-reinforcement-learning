7b17e321485bcf70288f8aded23a26587b034f59
cph <stegben.benjamin@gmail.com>
2017-06-19 10:30:14 +0800

    add linearOUputMLP

--- nn/mlp.py
+++ nn/mlp.py
@@ -22,3 +22,24 @@ class MLP(nn.Module):
 
     def forward(self, x):
         return F.softmax(self.model.forward(x))
+
+
+class LinearOutputMLP(nn.Module):
+    def __init__(self, input_dim, hidden_structure, output_dim):
+        super(LinearOutputMLP, self).__init__()
+        all_layers_dim = [input_dim] + hidden_structure
+        layers = []
+
+        for idx in range(len(all_layers_dim) - 1):
+            n_in = all_layers_dim[idx]
+            n_out = all_layers_dim[idx + 1]
+
+            layers.append(nn.Linear(n_in, n_out))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(0.5))
+        layers.append(nn.Linear(n_out, output_dim))
+
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
